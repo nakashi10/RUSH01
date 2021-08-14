81e2b0c91db33bf53b435954a646883add4076f0
shunki.42 <o8084237368@gmail.com>
2021-08-14 21:21:43 +0900

    add first_input

---
+++ sshimoka/a.c
@@ -0,0 +1,19 @@
+void	first_input(int a[4][4][5])
+{
+	int	i;
+	int	j:
+	int	k;
+
+	i=-1;
+	while (++i < 4)
+	{
+		j=-1;
+		while (++j <4)
+		{
+			k = -1;
+			while (++k < 5)
+				if (k == 0)
+				a[i][j][k] = 0;
+			a[i][j][k] = 1;
+		}
+	}
