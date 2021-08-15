3adb0bdbd63a6d82614444935fbcc4d7987ec213
Kuon Tanigawa <kuonflower@gmail.com>
2021-08-16 00:13:52 +0900

    mv ex00 dir map_final_print.c map_scroll.c map_update.c

--- ex00/map_final_print.c
+++ ex00/map_final_print.c
@@ -7,28 +7,31 @@ char	g_final[3];
 
 void	map_final_print(int map[4][4][5], int size_i, int size_j, int size_k)
 {
-	g_i = -1;
-	g_j = -1;
-	g_k = 0;
+	g_i = 0;
+	g_j = 0;
+	g_k = 1;
 	g_final[1] = ' ' ;
 	g_final[2] = '\n';
-	while (g_i++ < size_i)
+	while (g_i < size_i)
 	{
-		while (g_j++ < size_j)
+		while (g_j < size_j)
 		{
-			while (g_k++ < size_k)
+			while (g_k < size_k)
 			{
-				if (map[i][j][k] == 1)
+				if (map[g_i][g_j][g_k] == 1)
 				{
-					g_final[0] = '0' + k;
+					g_final[0] = '0' + g_k;
 					write(1, &g_final[0], 1);
 					break ;
 				}
+				g_k++;
 			}
 			g_k = 1;
 			write(1, &g_final[1], 1);
+			g_j++;
 		}
 		write(1, &g_final[2], 1);
 		g_j = 0;
+		g_i++;
 	}
 }
