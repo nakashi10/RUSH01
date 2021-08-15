11b61aeb812f6bb2a52bd616d01c20513e3d55c1
Kuon Tanigawa <kuonflower@gmail.com>
2021-08-15 15:20:42 +0900

    update map_print.c map_final_print.c map_update.c

---
+++ ktanigaw/ktani_main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	map_final_print(int map[4][4][5],int size_i,int size_j,int size_k);
+void	map_print(int map[4][4][5], int size_i, int size_j, int size_k);
+void	map_update(int map[4][4][5], int size_i, int size_j, int size_k);
+
+int	main(int argc, char *argv[])
+{
+
+//argsの文字列を読み取って要素数を調べてくるメソッド必要
+int map[4][4][5];
+
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+				printf("test");
+
+	int size_i = sizeof map / sizeof map[0];
+	int size_j = sizeof map / sizeof map[0][0] / size_i;
+	int size_k = sizeof map / sizeof map[0][0][0] / size_i / size_j;
+	while (i < size_i)
+	{
+		while (j < size_j)
+		{
+			while (k < size_k)
+			{
+				if(k == 0)
+				{
+				map[i][j][k] = 0;
+				} else
+				map[i][j][k] = 1;
+				k++;
+			}
+			k = 0;
+			j++;
+		}
+		j = 0;
+		i++;
+
+	}
+	map_print(map, size_i, size_j, size_k);
+	map_update(map, size_i, size_j, size_k);
+	map_final_print(map, size_i, size_j, size_k);
+}
--- ktanigaw/map_print.c
+++ ktanigaw/map_print.c
@@ -1,28 +1,23 @@
 	#include <stdio.h>
 
-	void	map_print(int map[4][4][5])
+	void	map_print(int map[4][4][5], int size_i, int size_j, int size_k)
 	{
-
 	int i;
 	int j;
 	int k;
-	int size_i;
-	int size_j;
-	int size_k;
 
 	i = 0;
 	j = 0;
 	k = 0;
-	size_i = sizeof map / sizeof map[0];
-	size_j = sizeof map / sizeof map[0][0] / size_i;
-	size_k = sizeof map / sizeof map[0][0][0] / size_i / size_j;
+	printf("map[%d][%d][%d] no nakami ha %d\n", i, j , k , map[i][j][k]);
+
 	while (i < size_i)
 	{
 		while (j < size_j)
 		{
 			while (k < size_k)
 			{
-				printf("a[%d][%d][%d] no nakami ha %d\n", i, j , k , a[i][j][k]);
+				printf("map[%d][%d][%d] no nakami ha %d\n", i, j , k , map[i][j][k]);
 				k++;
 			}
 			k = 0;
