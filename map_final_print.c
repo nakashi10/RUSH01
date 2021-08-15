2e85f5cb929396347b3d00e6465d84dc567ecab8
Kuon Tanigawa <kuonflower@gmail.com>
2021-08-15 22:28:45 +0900

    update

--- map_final_print.c
+++ map_final_print.c
@@ -1,38 +1,34 @@
 #include <unistd.h>
-#include <stdio.h>
 
-void	map_final_print(int map[4][4][5],int size_i,int size_j,int size_k)
+int		g_i = -1;
+int		g_j = -1;
+int		g_k = 0;
+char	g_final[3];
+
+void	map_final_print(int map[4][4][5], int size_i, int size_j, int size_k)
 {
-    int		i;
-	int		j;
-	int		k;
-	char	final[3];
-
-	i = 0;
-	j = 0;
-	k = 1;
-	final[1] = ' ' ;
-	final[2] = '\n';
-	while (i < size_i)
+	g_i = -1;
+	g_j = -1;
+	g_k = 0;
+	g_final[1] = ' ' ;
+	g_final[2] = '\n';
+	while (g_i++ < size_i)
 	{
-		while (j < size_j)
+		while (g_j++ < size_j)
 		{
-			while (k < size_k)
+			while (g_k++ < size_k)
 			{
-				if(map[i][j][k] == 1)
+				if (map[i][j][k] == 1)
 				{
-					final[0] = '0' + k;
-					write(1,&final[0],1);
-					break;
+					g_final[0] = '0' + k;
+					write(1, &g_final[0], 1);
+					break ;
 				}
-				k++;
 			}
-			k = 1;
-			j++;
-			write(1,&final[1],1);
+			g_k = 1;
+			write(1, &g_final[1], 1);
 		}
-		write(1,&final[2],1);
-		j = 0;
-		i++;
+		write(1, &g_final[2], 1);
+		g_j = 0;
 	}
 }
