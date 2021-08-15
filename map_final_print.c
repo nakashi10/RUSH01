db5c12d53ef6607d729a20438a14845aeebefcdd
yunakash <nakashi1000@gmail.com>
2021-08-15 21:21:33 +0900

    error check

--- map_final_print.c
+++ map_final_print.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	map_final_print(int map[4][4][5],int size_i,int size_j,int size_k)
+void	map_final_print(int map[4][4][5], int size_i, int size_j, int size_k)
 {
-    int		i;
+	int		i;
 	int		j;
 	int		k;
 	char	final[3];
@@ -19,19 +19,19 @@ void	map_final_print(int map[4][4][5],int size_i,int size_j,int size_k)
 		{
 			while (k < size_k)
 			{
-				if(map[i][j][k] == 1)
+				if (map[i][j][k] == 1 )
 				{
 					final[0] = '0' + k;
-					write(1,&final[0],1);
-					break;
+					write(1, &final[0], 1);
+					break ;
 				}
 				k++;
 			}
 			k = 1;
 			j++;
-			write(1,&final[1],1);
+			write(1, &final[1], 1);
 		}
-		write(1,&final[2],1);
+		write(1, &final[2], 1);
 		j = 0;
 		i++;
 	}
